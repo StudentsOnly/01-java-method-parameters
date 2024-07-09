5429597800c02e13b7c5d1b52ffc7a44cb395c6e
bruch-alex <alex.bruch@tutanota.com>
2024-07-09 14:34:01 +0200

    add solution

--- RectangleCalculator.java
+++ RectangleCalculator.java
@@ -1,3 +1,22 @@
-public class RectangleCalculator{
+import java.util.Scanner;
 
+public class RectangleCalculator{
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the length of the rectangle: ");
+        int length = s.nextInt();
+        System.out.println("Enter the width of the rectangle: ");
+        int width = s.nextInt();
+
+        System.out.printf("Area: %d\nPerimeter %d",calculateArea(length, width), calculatePerimeter(length, width));
+    }
+
+    static int calculateArea(int length, int width){
+        return length * width;
+    }
+
+    static int calculatePerimeter(int length, int width){
+        return (length + width) * 2;
+    }
 }
